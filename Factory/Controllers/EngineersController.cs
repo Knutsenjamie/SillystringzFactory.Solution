ab4ec392fdbb82b6abfa1b019b0989946ae54f79
“Jamie <knutsenjamie@yahoo.com>
2021-05-28 08:51:47 -0700

    add index methods to all controller types

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Factory.Controllers
+{
+  public class EngineersController : Controller
+  {
+    public ActionResult Index()
+    {
+      return View(_db.Engineers.ToList());
+    }
